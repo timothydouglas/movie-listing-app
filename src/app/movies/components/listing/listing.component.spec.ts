0276dc84464cbd1ddd70a70ac762c50a34596003
Tim Douglas <timothydouglas@live.com>
2020-11-11 03:30:10 +0000

    Tests

--- src/app/movies/components/listing/listing.component.spec.ts
+++ src/app/movies/components/listing/listing.component.spec.ts
@@ -1,25 +1,43 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 import { ListingComponent } from './listing.component';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
+import { Store } from '@ngrx/store';
+import * as fromStore from '../../store';
 
-describe('ListingComponent', () => {
+describe( 'ListComponent', () => {
   let component: ListingComponent;
   let fixture: ComponentFixture<ListingComponent>;
+  let store: MockStore<fromStore.AppState>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ListingComponent ]
-    })
-    .compileComponents();
-  });
+  beforeEach( async( () => {
+    TestBed.configureTestingModule( {
+      declarations: [
+        ListingComponent,
+      ],
+      imports: [
+        ScrollingModule,
+      ],
+      providers: [
+        provideMockStore(),
+      ]
+    } )
+      .compileComponents();
+  } ) );
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ListingComponent);
+  beforeEach( () => {
+    fixture = TestBed.createComponent( ListingComponent );
+    store = TestBed.get( Store );
+
+    store.overrideSelector( fromStore.getMoviesLoading, false );
+    store.overrideSelector( fromStore.getMovies, [] );
+    store.overrideSelector( fromStore.selectPage, 0 );
+
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  } );
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  it( 'should create', () => {
+    expect( component ).toBeTruthy();
+  } );
+} );
