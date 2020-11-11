56d8274ca54d3d83a2230497687714790392e2f3
Tim Douglas <timothydouglas@live.com>
2020-11-11 03:59:10 +0000

    Cleanup

--- src/app/shared/interceptors/content.interceptor.ts
+++ src/app/shared/interceptors/content.interceptor.ts
@@ -2,16 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, finalize } from 'rxjs/operators';
-import { MeterService } from '../services/meter/meter.service';
 
 @Injectable()
 export class ContentInterceptor implements HttpInterceptor {
-  constructor(
-    private meter: MeterService
-  ) {}
+  constructor() {}
 
   intercept( request: HttpRequest<any>, next: HttpHandler ): Observable<HttpEvent<any>> {
-    this.meter.launch( true );
     // add content-type header
     const req = request.clone( {
       setHeaders: {
@@ -24,7 +20,6 @@ export class ContentInterceptor implements HttpInterceptor {
         return throwError( error );
       } ) ).pipe(
       finalize( () => {
-        this.meter.launch( false );
     } ) ) as any;
   }
 }
