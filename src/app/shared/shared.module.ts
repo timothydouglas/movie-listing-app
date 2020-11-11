1cc49f356eb0526d843dcdd45908d56acaa69a32
Tim Douglas <timothydouglas@live.com>
2020-11-11 03:32:04 +0000

    Cleanup

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,15 +3,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
-import { MeterComponent } from './components/meter/meter.component';
-import { MeterService } from './services/meter/meter.service';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { ContentInterceptor } from './interceptors/content.interceptor';
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    MeterComponent
   ],
   imports: [
     CommonModule,
@@ -19,11 +16,9 @@ import { ContentInterceptor } from './interceptors/content.interceptor';
     RouterModule
   ],
   exports: [
-    HeaderComponent,
-    MeterComponent,
+    HeaderComponent
   ],
   providers: [
-    MeterService,
     { provide: HTTP_INTERCEPTORS, useClass: ContentInterceptor, multi: true },
   ]
 })
