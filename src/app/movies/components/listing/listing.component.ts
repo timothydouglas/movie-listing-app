177093607476437448012ce4813e0b94a7c775fd
Tim Douglas <timothydouglas@live.com>
2020-11-15 19:22:53 +0000

    Install ngx-componentdestroyed
    * Install ngx-componentdestroyed 5.0.2
    * Handle unsubscribe from resizeHandler observable with takeUntil
    * Handle ngFor loop with trackBy

--- src/app/movies/components/listing/listing.component.ts
+++ src/app/movies/components/listing/listing.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input, Output, EventEmitter, ViewChild } from '@angular/core';
-import { Movies } from '../../interfaces';
 import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+import { componentDestroyed, OnDestroyMixin } from '@w11k/ngx-componentdestroyed';
 import { fromEvent } from 'rxjs';
-import { debounceTime, distinctUntilChanged, tap } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, takeUntil, tap } from 'rxjs/operators';
+import { Movies } from '../../interfaces';
+import { ListRange } from '@angular/cdk/collections';
 
 @Component({
   selector: 'app-listing',
   templateUrl: './listing.component.html',
   styleUrls: ['./listing.component.scss']
 })
-export class ListingComponent {
+export class ListingComponent extends OnDestroyMixin {
 
   @ViewChild(CdkVirtualScrollViewport, { static: true }) viewport: CdkVirtualScrollViewport;
   @Output() loadMore: EventEmitter<null> = new EventEmitter();
@@ -20,19 +22,25 @@ export class ListingComponent {
   resizeHandler$ = fromEvent(window, 'resize').pipe(
     distinctUntilChanged(),
     debounceTime(60),
-    tap(() => this.viewport.checkViewportSize())
+    tap(() => this.viewport.checkViewportSize()),
+    takeUntil(componentDestroyed(this))
   );
 
-  scrolledIndexChange( index: number ) {
+  scrolledIndexChange( index: number ): void {
     if ( this.isLoading || !this.movies || this.movies.length === 0 ) {
       return;
     }
 
-    const { end } = this.viewport.getRenderedRange();
-    const total = this.viewport.getDataLength();
+    const { end }: ListRange = this.viewport.getRenderedRange();
+    const total: number = this.viewport.getDataLength();
 
     if ( end >= total - this.loadMoreBuffer ) {
       this.loadMore.emit();
     }
+  }
+
+
+  identify(index, movie): number {
+    return movie.id;
   }
 }
